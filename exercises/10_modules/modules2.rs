467317c543fea77c87a4c3a6e4b1af3b5a582b1f
Sayfulla Mirkhalikov <mirxoliqov.sayfulla@gmail.com>
2026-04-21 09:03:32 +0500

    Added solution of modules

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -2,9 +2,8 @@
 // the `use` and `as` keywords.
 
 mod delicious_snacks {
-    // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    pub use self::fruits::PEAR as fruits;
+    pub use self::veggies::CUCUMBER as veggies;
 
     mod fruits {
         pub const PEAR: &str = "Pear";
@@ -20,7 +19,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        delicious_snacks::fruits,
+        delicious_snacks::veggies,
     );
 }
