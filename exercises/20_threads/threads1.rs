1d1fe1b7371f9c6f7f58d2cad403f317b614a092
Sayfulla Mirkhalikov <mirxoliqov.sayfulla@gmail.com>
2026-06-14 08:17:46 +0500

    Added all solutions

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -20,10 +20,12 @@ fn main() {
         handles.push(handle);
     }
 
-    let mut results = Vec::new();
+    let mut results  = Vec::new();
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        let result = handle.join().unwrap();
+        results.push(result);
     }
 
     if results.len() != 10 {
--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -15,8 +15,8 @@ impl Queue {
 }
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
-    // TODO: We want to send `tx` to both threads. But currently, it is moved
-    // into the first thread. How could you solve this problem?
+    // Clone the sender so each thread owns its own handle to the channel.
+    let tx_clone = tx.clone();
     thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
@@ -28,7 +28,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     thread::spawn(move || {
         for val in q.second_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx_clone.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
